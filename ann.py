c667c4cda74865f1fcd86e6b8c912d89509f7ead
kevincwuu <kevincwuu@gmail.com>
2017-08-18 15:59:27 -0700

    Add data preprocessing, import dataset, encoding categorical data, splitting dataset, and feature scaling

--- ann.py
+++ ann.py
@@ -6,3 +6,47 @@ Created on Fri Aug 18 14:33:56 2017
 @author: kevinwu
 """
 
+# Part 1 - Data Preprocessing
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+# Encode before splitting because matrix X and independent variable Y must be already encoded
+# Found two categorical data (country, gender)
+# create dummy variables, avoid dummy varialbe trap
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+# lots of high computation to ease calculation, we don't want one independent variable dominating
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting classifier to the Training set
+# Create your classifier here
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
